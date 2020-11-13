db36d9607be87f9e5d5badcb9fc6e38a5ed2e351
Mohsen Shafiei <mohsen.shafieitafreshi@shopee.com>
2020-11-13 21:50:56 +0800

    fix: contains duplicate.

--- contains-duplicate/index.js
+++ contains-duplicate/index.js
@@ -1,13 +1,14 @@
 const arr = [1, 2, 3, 4, 5, 6]; // false
 const arr1 = [1, 2, 3, 4, 5, 6, 1]; // true
+const arr2 = [1, 2, 3, 1];
 
 const containDuplicate = (arr) => {
   let x = {};
   for (let i = 0; i < arr.length; i++) {
     if (x[arr[i]]) return true;
-    x = Object.assign(x, { [arr[i]]: i });
+    x[arr[i]] = 1;
   }
   return false;
 };
 
-console.log(containDuplicate(arr));
+console.log(containDuplicate(arr2));
