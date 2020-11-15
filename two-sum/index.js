797928282506f6ce801b8a4f152e6d2ea937dd79
Mohsen Shafiei <mohsen.shafieitafreshi@shopee.com>
2020-11-15 12:37:48 +0800

    feat: add two sum.

--- two-sum/index.js
+++ two-sum/index.js
@@ -1,14 +1,15 @@
-const arr = [1, 2, 4, 6];
-const target = 10;
+const arr = [1, 3, 4, 2];
+const target = 6;
 
 const twoSum = (arr, target) => {
-  const x = arr.reduce((res, el, index) => {
-    return { ...res, [el]: index };
-  }, {});
+  let x = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (!x[arr[i]]) x[arr[i]] = i;
+  }
   for (let i = 0; i < arr.length; i++) {
     const z = target - arr[i];
-    if (x[z]) return [i, x[z]];
+    if (x[z] && x[z] !== i) return [i, x[z]];
   }
 };
 
-twoSum(arr, target);
+console.log(twoSum(arr, target));
